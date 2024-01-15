2f9b1050075eb001026e19b9b49d6860a53a1c78
GopiChandAkkala <akkalagopi@gmail.com>
2024-01-16 01:21:17 +0530

    added vpc,eks modules

---
+++ eks.tf
@@ -0,0 +1,40 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+    
+
+  eks_managed_node_groups = {    
+    general = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  
+  
+}
---
+++ vpc.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = true
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {    
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
